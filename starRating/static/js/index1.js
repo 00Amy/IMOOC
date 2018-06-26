0d38444612cd0d42df960d4e3542b40587e449ad
yanting <819211036@qq.com>
2018-06-26 13:51:04 +0800

    添加了事件冒泡注释

--- starRating/static/js/index1.js
+++ starRating/static/js/index1.js
@@ -26,10 +26,16 @@
         lightOn($(this).index() + 1);
     }).on('click', function () {
         num = $(this).index() + 1;
+        /*事件冒泡在此体现为：点击每个星星时，父元素ul的事件也会被触发，
+        控制台会看到“子元素x被触发”+“父元素被触发”*/
+        // console.log('子元素' + $(this).index() + '被触发');
     });
 
     // 鼠标移出评分区域时恢复以num为准的评分
     $rating.on('mouseout', function () {
         lightOn(num);
     });
+    // $rating.on('click', function () {
+    //     console.log('父元素被触发');
+    // });
 });
