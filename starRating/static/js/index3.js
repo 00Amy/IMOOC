188227c7c342b043540b7f4de511a309165fb1f9
yanting <819211036@qq.com>
2018-06-27 15:27:00 +0800

    完成了3.0版本的基础部分，不包括显示当前是第几颗星星

---
+++ starRating/static/js/index3.js
@@ -0,0 +1,62 @@
+var rating = (function  () {
+    // 默认参数
+    var defaults = {
+        num: 0,
+        readOnly: false,
+        select: function () {},  //鼠标移上时
+        chosen: function () {}   //鼠标点击时
+    };
+
+    // 点亮整颗星星
+    var lightEntire = function (el, options) {
+        this.$el = $(el);
+        this.$item = this.$el.find('.rating-item');
+        this.opts = options;
+    };
+
+    // 编写lightEntire的初始化方法，一般将方法都写在构造函数的原型上，
+    // 以便无论实例化多少次内存中都只有一份
+    lightEntire.prototype.init = function () {
+        this.lightOn(this.opts.num);
+        this.bindEvents();
+    };
+    lightEntire.prototype.lightOn = function (num) {
+        num = parseInt(num);
+
+        this.$item.each(function (index) {
+            if (index < num) {
+                $(this).css('background-position', '0 -40px');
+            }
+            else {
+                $(this).css('background-position', '0 0');
+
+            }
+        });
+    };
+    lightEntire.prototype.bindEvents = function () {
+        self = this;
+
+        self.$el.on('mouseover', '.rating-item', function () {
+            self.lightOn($(this).index() + 1);
+        }).on('click', '.rating-item', function () {
+            self.opts.num = $(this).index() + 1;
+        }).on('mouseout', function () {
+            self.lightOn(self.opts.num);
+        });
+    };
+
+    // 初始化方法，参数：id和用户传入的参数
+    var init = function (el, options) {
+        // 用options覆盖defaults，并赋给{}
+        options = $.extend({}, defaults, options);
+        new lightEntire(el, options).init();
+    };
+
+    return {
+        initFn: init
+    };
+})();
+
+rating.initFn('#rating3-1',{
+    num: 1
+});
